aa5e53106b3ed77b6317148d85e6d3e987e10823
Paul Downey <psd@whatfettle.com>
2022-04-28 20:42:10 +0100

    Made regex match optional

--- application/commands.py
+++ application/commands.py
@@ -57,7 +57,12 @@ def _run_tests():
                 parsed = JSONPath(path).parse(data)
                 if parsed:
                     actual = parsed[0]
-                    match = True if re.match(str(expected), str(actual)) else False
+                    if str(expected.startswith("~")):
+                        match = (
+                            True if re.match(str(expected[1:]), str(actual)) else False
+                        )
+                    else:
+                        match = str(expected) == str(actual)
                     assertion = Assertion(
                         path=path,
                         expected=expected,
