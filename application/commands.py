5119ece38e5950f431f6fbdc8e5f61feea0c19c2
Adam Shimali <adam.shimali@gmail.com>
2022-05-13 09:12:50 +0100

    Run get requests in threadpool to speed up tests.

--- application/commands.py
+++ application/commands.py
@@ -1,3 +1,4 @@
+import concurrent
 import datetime
 import re
 
@@ -23,75 +24,93 @@ def _run_tests():
 
     print(f"Running tests at {datetime.datetime.utcnow()}")
     results = []
-    for la in local_authorities:
-
-        for test, details in tests[la].items():
-            print(f"Running test: {test}")
-
-            dataset = details.get("dataset")
-            query = details.get("query")
-            assertions = details.get("assertions", {})
-            warnings = details.get("warnings", {})
-            ticket = details.get("ticket")
-
-            checks = {"strict": assertions, "warning": warnings}
-
-            test_url = f"{BASE_API_URL}{query}"
-            resp = requests.get(test_url)
-            resp.raise_for_status()
-            data = resp.json()
-
-            # sort by entity id to give tests a predicable ordering
-            if data["count"] > 1 and data.get("entities"):
-                data["entities"].sort(key=lambda e: e["entity"])
-
-            response_data = ResponseData(query=query, test_name=test, data=data)
-            result = Result(
-                query=query,
-                organisation=la,
-                dataset=dataset,
-                test_name=test,
-                ticket=ticket,
-            )
-            response_data.results.append(result)
-            results.append(result)
-
-            for level, checks in checks.items():
-                for path, expected in checks.items():
-                    print(f"path = {path} expect = {expected} : check {level}")
-                    parsed = JSONPath(path).parse(data)
-                    assertion_type = AssertionType(level)
-                    if parsed:
-                        actual = str(parsed[0])
-                        expected = str(expected)
-                        if expected.startswith("~"):
-                            match = True if re.match(expected[1:], actual) else False
-                        else:
-                            match = expected == actual
-                        assertion = Assertion(
-                            path=path,
-                            expected=expected,
-                            actual=actual,
-                            match=match,
-                            assertion_type=assertion_type,
-                        )
-                    else:
-                        assertion = Assertion(
-                            path=path,
-                            expected=expected,
-                            actual=None,
-                            match=None,
-                            assertion_type=assertion_type,
-                        )
-                    result.assertions.append(assertion)
-
-            db.session.add(response_data)
+    for local_authority in local_authorities:
+        results.extend(_run_tests_for_local_authority(local_authority, tests))
 
     test_run = TestRun(results=results)
     db.session.add(test_run)
     db.session.commit()
 
     print(f"Finished running tests at {datetime.datetime.utcnow()}")
+
+
+def _run_tests_for_local_authority(local_authority, tests):
+    results = []
+    test_names = []
+    test_details = []
+    for test_name, details in tests[local_authority].items():
+        test_names.append(test_name)
+        test_details.append(details)
+    local_authority_names = [local_authority]*len(test_names)
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for result in executor.map(_run_single_test, local_authority_names, test_names, test_details):
+            results.extend(result)
+        executor.shutdown()
+
+    return results
+
+
+def _run_single_test(local_authority, test_name, details):
+    print(f"Running test: {test_name}")
+    results = []
+    dataset = details.get("dataset")
+    query = details.get("query")
+    assertions = details.get("assertions", {})
+    warnings = details.get("warnings", {})
+    ticket = details.get("ticket")
+    checks = {"strict": assertions, "warning": warnings}
+    test_url = f"{BASE_API_URL}{query}"
+    try:
+        resp = requests.get(test_url)
+        resp.raise_for_status()
+    except Exception as e:
+        print(e)
+        return results
+    data = resp.json()
+    # sort by entity id to give tests a predicable ordering
+    if data["count"] > 1 and data.get("entities"):
+        data["entities"].sort(key=lambda e: e["entity"])
+    response_data = ResponseData(query=query, test_name=test_name, data=data)
+    result = Result(
+        query=query,
+        organisation=local_authority,
+        dataset=dataset,
+        test_name=test_name,
+        ticket=ticket,
+    )
+    response_data.results.append(result)
+    results.append(result)
+    for level, checks in checks.items():
+        for path, expected in checks.items():
+            print(f"path = {path} expect = {expected} : check {level}")
+            parsed = JSONPath(path).parse(data)
+            assertion_type = AssertionType(level)
+            if parsed:
+                actual = str(parsed[0])
+                expected = str(expected)
+                if expected.startswith("~"):
+                    match = True if re.match(expected[1:], actual) else False
+                else:
+                    match = expected == actual
+                assertion = Assertion(
+                    path=path,
+                    expected=expected,
+                    actual=actual,
+                    match=match,
+                    assertion_type=assertion_type,
+                )
+            else:
+                assertion = Assertion(
+                    path=path,
+                    expected=expected,
+                    actual=None,
+                    match=None,
+                    assertion_type=assertion_type,
+                )
+            result.assertions.append(assertion)
+
+    return results
 
 
 @data_test_cli.command("delete-old-tests")
